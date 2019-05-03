c9a6cf5cca52a7b65e205337b531e3e9e09b0b4a
William Sinn <w.h.sinn@gmail.com>
2019-05-03 14:58:09 -0400

    finished crud for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,19 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     redirect_to @user
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+  end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
   private
     def user_params
       params.require(:user).permit(:name, :age)
