a3c2e16d7429d445cf6ad8d6fa3fec5cfbc307cf
William Sinn <w.h.sinn@gmail.com>
2019-05-03 15:47:15 -0400

    add marathon seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Marathon.find_or_create_by(name:"Boston Marathon", location:"Boston, MA", date: 12342341, m_type: "full-marathon")
+Marathon.find_or_create_by(name:"Cali Marathon", location:"California", date: 15345342, m_type: "half-marathon")
