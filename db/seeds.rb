3dd7244c7faf30d59fb3f81252a2204a62047cf5
axionos <axionoso@gmail.com>
2019-05-03 14:37:03 -0400

    create seed data for Funds table and updated Funds index.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Fund.create!(name:"UNICEF USA", description: "UNICEF works in more than 190 countries and territories to put children first and has helped save more children’s lives than any other humanitarian organization. By joining Team UNICEF YOU will help support the world’s children and together, we will work towards a day where every child has a safe and healthy childhood.")
+Fund.create!(name:"Alzheimer's Association", description: "The Alzheimer's Association is the leading voluntary health organization in Alzheimer's care, support and research. To eliminate Alzheimer's Disease through the advancement of research; to provide and enhance care and support for all those affected; and to reduce risk of dementia through the promotion of brain health.")
+Fund.create!(name:"Autism Speaks", description: "Autism Speaks is dedicated to solutions for individuals with autism and their families through advocacy, support and advancing research into causes and better interventions for autism spectrum disorder and related conditions, while increasing understanding and acceptance of people with autism spectrum disorder.")
