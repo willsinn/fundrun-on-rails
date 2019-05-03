79a2dfed39b08d478214ab327a2aa3edbc35a759
William Sinn <w.h.sinn@gmail.com>
2019-05-03 14:35:23 -0400

    add user new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
   def index
-  end
-
-  def new
-  end
-
-  def create
-
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :age)
+    end
+
 end
