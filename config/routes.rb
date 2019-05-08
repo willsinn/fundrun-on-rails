c686d2710568675fc5c6da0de8a1d9f7bfb7e761
axionos <axionoso@gmail.com>
2019-05-08 13:28:03 -0400

    style signup form

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :funds
   resources :users, except: [:new]
-  resources :sessions, only: [:create]
+  resources :sessions, only: [:new,:create]
   resources :donations
   resources :marathons
   resources :participations
