4ba9d44ee9ae0c7327a86be9f466797abf358bde
William Sinn <w.h.sinn@gmail.com>
2019-05-08 14:38:35 -0400

    add funds & users relationships, add columns to funds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :donations
   has_many :participations
   has_many :marathons, through: :participations
+  has_many :funds, through: :donations
   # validates :name, presence: true, uniqueness: true
 
 
--- db/migrate/20190503174434_create_funds.rb
+++ db/migrate/20190503174434_create_funds.rb
@@ -2,6 +2,8 @@ class CreateFunds < ActiveRecord::Migration[5.2]
   def change
     create_table :funds do |t|
       t.string :name
+      t.string :site
+      t.string :img_link
       t.string :description
 
       t.timestamps
