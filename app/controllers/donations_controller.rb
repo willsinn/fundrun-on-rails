25bcdf1ad9178f92f58fc5821232c01a418c4c60
William Sinn <w.h.sinn@gmail.com>
2019-05-08 21:59:22 -0400

    add create donate, routes-flash/redirects work

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,18 +1,27 @@
 class DonationsController < ApplicationController
   def new
-    byebug
     @donation = Donation.new
-
   end
 
   def create
-    @donation = Donation.create(donation_params)
-    # redirect_to funds_index
+    donate = params.require(:donation).permit(:amount, :fund_id)
+    donate_amount = donate[:amount].to_i
+    donate_fund_id = donate[:fund_id].to_i
+    @donation = Donation.create(amount: donate_amount, user_id: session[:user_id], fund_id: donate_fund_id)
+
+
+    @funds = Fund.all
+    @fund = @funds.find_by(id: donate_fund_id)
+
+    redirect_to funds_path
+    flash[:notice] = "Thank you for donating $#{donate_amount} to #{@fund.name}"
+
   end
 
-  private
-
-  def donation_params
-    params.require(:donation).permit(:amount, :user_id, :fund_id)
-  end
+  # private
+  #
+  # def donation_params
+  #   params.require(:donation).permit(:amount, :fund_id)
+  #
+  # end
 end
--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -9,8 +9,8 @@ class FundsController < ApplicationController
 
   def create
     byebug
-    donate_fund_id = params.require(:id).to_i
-    @donation = Donation.create(amount: 10, user_id: session[user_id], fund_id: donate_fund_id )
+    # donate_fund_id = params.require(:id).to_i
+    # @donation = Donation.create(amount: 10, user_id: session[user_id], fund_id: donate_fund_id )
 
   end
 end
