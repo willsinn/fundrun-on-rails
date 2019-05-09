668a57667e82fb78ca1ae8695f61cef702b576a5
axionos <axionoso@gmail.com>
2019-05-09 18:29:19 -0400

    working copy with charity route fixed

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -4,19 +4,18 @@ class FundsController < ApplicationController
   end
 
   def show
-    @funds = Fund.all
+    # @funds = Fund.all
     @fund = Fund.find(params[:id])
-
     @donations = Donation.all
     fund_donations = @donations.select {|donation| donation.fund_id == @fund.id}
     total_amount = 0
-
-    if fund_donations.length >= 1
-    fund_donations.each do |fund_donation|
-      amt = fund_donation.amount
-      total_amount+=amt
-    end
-    total_amount
+    # byebug
+    if fund_donations.length >= 0
+      fund_donations.each do |fund_donation|
+        amt = fund_donation.amount
+        total_amount+=amt
+      end
+      total_amount
     else
       total_amount = fund_donation.amount
     end
